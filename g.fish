ac0fbc0c278a93eae6ab2d3252fb05ef52cc4909
dickeyxxx <jeff@dickeyxxx.com>
2014-07-31 20:11:37 -0700

    added autocompletion

--- g.fish
+++ g.fish
@@ -23,3 +23,16 @@ function g
 
   cd $path
 end
+
+function __g_user_completion
+  ls $HOME/src/github.com
+end
+
+function __g_repo_completion
+  set -l cmd (commandline -o)
+  set -l user $cmd[2]
+  ls $HOME/src/github.com/$user
+end
+
+complete -c g -n '__fish_is_token_n 2' --arguments '(__g_user_completion)' --no-files
+complete -c g -n '__fish_is_token_n 3' --arguments '(__g_repo_completion)' --no-files
