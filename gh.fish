00dfa054ea3b8c4cbfad3e42e6876553dd25d831
Jeff Dickey <jeff@dickeyxxx.com>
2014-08-01 21:05:59 -0700

    g -> gh rename

--- gh.fish
+++ gh.fish
@@ -1,4 +1,4 @@
-function g
+function gh
   set -l user $GITHUB
   set -l repo ""
 
@@ -24,15 +24,15 @@ function g
   cd $path
 end
 
-function __g_user_completion
+function __gh_user_completion
   ls $HOME/src/github.com/$GITHUB
 end
 
-function __g_repo_completion
+function __gh_repo_completion
   set -l cmd (commandline -o)
   set -l user $cmd[2]
   ls $HOME/src/github.com/$user
 end
 
-complete -c g -n '__fish_is_token_n 2' --arguments '(__g_user_completion)' --no-files
-complete -c g -n '__fish_is_token_n 3' --arguments '(__g_repo_completion)' --no-files
+complete -c gh -n '__fish_is_token_n 2' --arguments '(__gh_user_completion)' --no-files
+complete -c gh -n '__fish_is_token_n 3' --arguments '(__gh_repo_completion)' --no-files
