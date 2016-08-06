f51e91d745d9ce011f7c6399e2864ee1a07caf12
Jonathan King <JonathanUsername@users.noreply.github.com>
2016-08-06 01:16:17 +0100

    Add bash completion (#18)

---
+++ completions/gh.bash
@@ -0,0 +1,18 @@
+# Add this to your .bashrc
+_complete_gh ()
+{
+        COMPREPLY=()
+        if [[ $COMP_CWORD -eq 1 ]]; then
+          comp_arr=$(ls $HOME/src/github.com;\
+            ls $HOME/src/github.com/$GITHUB)
+        elif [[ $COMP_CWORD -eq 2 ]]; then
+          local user=${COMP_WORDS[COMP_CWORD-1]}
+          comp_arr=$(ls $HOME/src/github.com/$user)
+        else
+          return 0
+        fi
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        COMPREPLY=( $(compgen -W "${comp_arr}" -- $cur))
+        return 0
+}
+complete -F _complete_gh gh
