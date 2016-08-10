2c5588de761b7399586b9e9a12a48475b1a4d8ca
Shane Barratt <stbarratt@gmail.com>
2016-08-09 20:37:03 -0400

    fixed fish scripts (#23)

--- functions/bb.fish
+++ functions/bb.fish
@@ -1,4 +1,7 @@
-GH_BASE_DIR=${GH_BASE_DIR:-$HOME/src}
+if not set -q BB_BASE_DIR
+    set BB_BASE_DIR $HOME/src
+end
+
 function bb
   set git_host bitbucket.org
   set -l repo ""
@@ -11,7 +14,7 @@ function bb
   set user $argv[1]
   set repo $argv[2]
 
-  set -l path $GH_BASE_DIR/$git_host/$user/$repo
+  set -l path $BB_BASE_DIR/$git_host/$user/$repo
   if not test -d $path
     git clone git@$git_host:$user/$repo.git $path
   end
