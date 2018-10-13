b34f570e57d4727a8d69dbe5f98ae6147160a52e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-10-13 16:16:25 -0700

    fix api calls

--- completions/bb.fish
+++ completions/bb.fish
@@ -10,14 +10,19 @@ end
 function __bb_repo_completion
   set -l cmd (commandline -o)
   set -l user $cmd[2]
-  set -l repos (command ls -L $BB_BASE_DIR/$GIT_HOST/$user)
-  # if 'jq' is available and remote repository cache is not set yet
-  if type -q jq; and not set -q BB_REPOS_CACHE
-    set -l repos_url https://api.bitbucket.org/2.0/repositories/$user
-    set -xU BB_REPOS_CACHE (curl --silent $repos_url | jq -r '.values[] | select(.scm=="git") | .name')
+  set -l cache_path BB_REPOS_CACHE_$user
+  set -l repo_dir $BB_BASE_DIR/$GIT_HOST/$user
+  set -l repos ""
+
+  if test -d $repo_dir
+    set repos (command ls -L $repo_dir)
   end
-  set repos $repos $BB_REPOS_CACHE
-  printf "%s\n" $repos | sort | uniq -du
+  # if 'jq' is available and remote repository cache is not set yet
+  if type -q jq; and not set -q $cache_path
+    set -l repos_url https://api.bitbucket.org/2.0/repositories/$user
+    set -xU $cache_path (curl --silent $repos_url | jq -r '.values[] | select(.scm=="git") | .name')
+  end
+  printf "%s\n%s\n" $$cache_path $repos | sort | uniq -du
 end
 
 complete -c bb -n '__fish_is_token_n 2' --arguments '(__bb_user_completion)' --no-files
--- completions/gh.fish
+++ completions/gh.fish
@@ -9,14 +9,20 @@ end
 function __gh_repo_completion
   set -l cmd (commandline -o)
   set -l user $cmd[2]
-  set -l repos (command ls -L $GH_BASE_DIR/github.com/$user)
-  # if 'jq' is available and remote repository cache is not set yet
-  if type -q jq; and not set -q GH_REPOS_CACHE
-    set -l repos_url https://api.github.com/users/$user/repos
-    set -xU GH_REPOS_CACHE (curl --silent $repos_url | jq -r '.[] | "\(.name)"')
+  set -l cache_path GH_REPOS_CACHE_$user
+  set -l repo_dir $GH_BASE_DIR/github.com/$user
+  set -l repos ""
+
+  if test -d $repo_dir
+    set repos (command ls -L $repo_dir)
   end
-  set repos $repos $GH_REPOS_CACHE
-  printf "%s\n" $repos | sort | uniq -du
+
+  # if 'jq' is available and remote repository cache is not set yet
+  if type -q jq; and not set -q $cache_path
+    set -l repos_url https://api.github.com/users/$user/repos
+    set -xU $cache_path (curl --silent $repos_url | jq -r '.[] | "\(.name)"')
+  end
+  printf "%s\n%s\n" $$cache_path $repos | sort | uniq -du
 end
 
 complete -c gh -n '__fish_is_token_n 2' --arguments '(__gh_user_completion)' --no-files
