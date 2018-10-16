f517d1509ecc7be62a36294a0acf858e330bb1ab
Vitaliy Tsvayer <tsvayer@gmail.com>
2018-10-16 17:56:38 +0300

    add parent repository as upstream remote

--- functions/bb.fish
+++ functions/bb.fish
@@ -2,22 +2,34 @@ if not set -q BB_BASE_DIR
     set BB_BASE_DIR $HOME/src
 end
 
-function bb
-  set git_host bitbucket.org
-  set -l repo ""
+function __bb_add_upstream_remote --argument repo
+    set -l url https://api.bitbucket.org/2.0/repositories/$repo
+    set -l parent_repo (curl -s $url | jq -r 'select(.parent != null) | .parent | .full_name')
+    if test -n "$parent_repo"
+        git remote add upstream git@bitbucket.org:$parent_repo.git
+    end
+end
 
-  if [ (count $argv) -ne 2 ]
-    echo "USAGE: bb [user] [repo]"
-    return -1
-  end
+function bb -d "manage git repos"
+    set git_host bitbucket.org
+    set -l repo ""
 
-  set user $argv[1]
-  set repo $argv[2]
+    if [ (count $argv) -eq 1 ]
+        set repo $argv[1]
+    else if [ (count $argv) -eq 2 ]
+        set repo $argv[1]/$argv[2]
+    else
+        echo "USAGE: bb [user] [repo]"
+        return -1
+    end
 
-  set -l path $BB_BASE_DIR/$git_host/$user/$repo
-  if not test -d $path
-    git clone git@$git_host:$user/$repo.git $path
-  end
+    set -l path $BB_BASE_DIR/$git_host/$repo
+    if not test -d $path
+        git clone git@$git_host:$repo.git $path
+        cd $path
+        and git branch --set-upstream-to=origin/master master
+        and __bb_add_upstream_remote $repo
+    end
 
-  cd $path
+    cd $path
 end
--- functions/gh.fish
+++ functions/gh.fish
@@ -2,24 +2,34 @@ if not set -q GH_BASE_DIR
     set GH_BASE_DIR $HOME/src
 end
 
+function __gh_add_upstream_remote --argument repo
+    set -l url https://api.github.com/repos/$repo
+    set -l parent_repo (curl -s $url | jq -r 'select(.parent != null) | .parent | .full_name')
+    if test -n "$parent_repo"
+        git remote add upstream git@github.com:$parent_repo.git
+    end
+end
+
 function gh -d "manage git repos"
-  set git_host github.com
-  set -l repo ""
+    set git_host github.com
+    set -l repo ""
 
-  if [ (count $argv) -eq 1 ]
-    set repo $argv[1]
-  else if [ (count $argv) -eq 2 ]
-    set repo $argv[1]/$argv[2]
-  else
-    echo "USAGE: gh [user] [repo]"
-    return -1
-  end
+    if [ (count $argv) -eq 1 ]
+        set repo $argv[1]
+    else if [ (count $argv) -eq 2 ]
+        set repo $argv[1]/$argv[2]
+    else
+        echo "USAGE: gh [user] [repo]"
+        return -1
+    end
 
-  set -l path $GH_BASE_DIR/$git_host/$repo
-  if not test -d $path
-    git clone git@$git_host:$repo.git $path
-    cd $path; and git branch --set-upstream-to=origin/master master
-  end
+    set -l path $GH_BASE_DIR/$git_host/$repo
+    if not test -d $path
+        git clone git@$git_host:$repo.git $path
+        cd $path
+        and git branch --set-upstream-to=origin/master master
+        and __gh_add_upstream_remote $repo
+    end
 
-  cd $path
+    cd $path
 end
